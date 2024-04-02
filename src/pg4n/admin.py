7d055b9a531be8280993e3f0c8fa375677ecfdf8
Ameer Kulabi <ameerhaider1394@gmail.com>
2024-04-02 19:57:06 +0300

    Remove not used code

--- src/pg4n/admin.py
+++ src/pg4n/admin.py
@@ -1,23 +1,12 @@
 
 command_list = ["help", "exit"]
 
-def activate():
-    print("API activated")
-
-def deactivate():
-    print("API activated")
-
 def main():
-    
     while(True):
         command = input("Enter command: ")
         match command:
             case "help":
                 print(command_list)
-            case "activate":
-                activate()
-            case "deactivate":
-                deactivate()
             case "exit":
                 break
             case _:
